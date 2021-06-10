18ff6a1628d1021276d462857708c0effb100655
ITTECH-DEO <etechproffit@gmail.com>
2021-06-10 17:16:56 +0700

    Perubahan pada loginpage

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'SignInPage.dart';
 import 'HomePage.dart';
+import 'SignUpPages.dart';
+import 'SignInPage.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,62 +9,143 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool _isHidePassword = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isHidePassword = !_isHidePassword;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+        backgroundColor: Colors.teal[600],
+      ),
       body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlutterLogo(size: 150),
-              SizedBox(height: 50),
-              _signInButton(),
-            ],
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img1.png'),
+            fit: BoxFit.cover,
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _signInButton() {
-    return OutlineButton(
-      splashColor: Colors.grey,
-      onPressed: () {
-        signInWithGoogle().then((result) {
-          if (result != null) {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return HomePage();
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              child: TextField(
+                controller: emailController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                ),
+                onChanged: (value) {
+                  //
                 },
               ),
-            );
-          }
-        });
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-      highlightElevation: 0,
-      borderSide: BorderSide(color: Colors.grey),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            ),
             Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                'Sign in with Google',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
+              padding: const EdgeInsets.fromLTRB(30, 8, 30, 8),
+              child: TextFormField(
+                controller: passwordController,
+                obscureText: _isHidePassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40.0)),
+                  labelText: 'Password',
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      _togglePasswordVisibility();
+                    },
+                    child: Icon(
+                      _isHidePassword ? Icons.visibility_off : Icons.visibility,
+                      color: _isHidePassword ? Colors.white : Colors.white,
+                    ),
+                  ),
+                  filled: true,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Enter Password';
+                  } else if (value.length < 6) {
+                    return 'Password must be at least 6 characters!';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 40, 30, 60),
+              child: Container(
+                width: double.infinity,
+                height: 55,
+                child: RaisedButton(
+                  color: Colors.teal[600],
+                  child: Text(
+                    'Sign In',
+                    textScaleFactor: 1.5,
+                    style: TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  onPressed: () async {
+                    signIn(emailController.text, passwordController.text).then(
+                      (result) {
+                        if (result != null) {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return HomePage();
+                              },
+                            ),
+                          );
+                        }
+                      },
+                    );
+                  },
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40)),
                 ),
               ),
-            )
+            ),
+            SizedBox(height: 40),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(bottom: 40),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    "Don’t have account ?",
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpPage()));
+                    },
+                    child: Text(
+                      "Register here",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
