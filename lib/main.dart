00a8d941efb75717b93408be77a24e0c46a81797
erlangga11 <norang178@gmail.com>
2021-06-20 10:38:20 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/HomePage.dart';
+import 'pages/LoginPage.dart';
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
