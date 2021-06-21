04189b5ee771868215754e770eca04ea18d31a48
ITTECH-DEO <etechproffit@gmail.com>
2021-06-21 13:53:07 +0700

    Fix In main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+import 'package:final_project/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
-import 'pages/HomePage.dart';
-import 'pages/LoginPage.dart';
+
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
-      home: HomePage(),
-          ));
-      
-      
+      home: SignInPage(),
+    ));
