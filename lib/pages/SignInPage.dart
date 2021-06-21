1a0c980d4753e0ef3a49c94f14a4cdceb55f8ee4
ITTECH-DEO <etechproffit@gmail.com>
2021-06-21 13:54:05 +0700

    Fix in Sig in

--- lib/pages/SignInPage.dart
+++ lib/pages/SignInPage.dart
@@ -6,6 +6,7 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 String name;
 String email;
+String password;
 String imageUrl;
 
 Future<void> signOutEmail() async {
@@ -17,40 +18,56 @@ Future<User> signInWithGoogle() async {
   await Firebase.initializeApp();
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
-  await googleSignInAccount.authentication;
+      await googleSignInAccount.authentication;
   final AuthCredential credential = GoogleAuthProvider.credential(
-  accessToken: googleSignInAuthentication.accessToken,
-  idToken: googleSignInAuthentication.idToken,
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
   );
 }
 
-Future<String> signUp(String emailInput, String password) async {
+Future<User> signUp(String email, String password) async {
   await Firebase.initializeApp();
   try {
-    // UserCredential userCredential = await FirebaseAuth.instance
-    //     .signInWithEmailAndPassword(email: emailInput, password: password);
-
-    // final User user = userCredential.user;
-
-    // if (user != null) {
-    //   assert(user.email != null);
-    //   email = user.email;
-    //   assert(!user.isAnonymous);
-    //   assert(await user.getIdToken() != null);
-    //   final User currentUser = _auth.currentUser;
-    //   assert(user.uid == currentUser.uid);
-    //   print('signInWithGoogle succeeded: $user');
-    //   return '$user';
-    // }
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'user-not-found') {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: emailInput, password: password);
-      return await signUp(emailInput, password);
-    } else if (e.code == 'wrong-password') {
-      print('Wrong password provided for that user.');
-      return null;
-    }
+    UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    User user = result.user;
+    assert(user != null);
+    assert(await user.getIdToken() != null);
+    return user;
+  } catch (e) {
+    print(e.toString());
+    return null;
   }
-  return null;
 }
+
+Future<User> signIn(String email, String password) async {
+  await Firebase.initializeApp();
+  try {
+    UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    User user = result.user;
+    assert(user != null);
+    assert(await user.getIdToken() != null);
+    return user;
+  } catch (e) {
+    print(e.toString());
+    return null;
+  }
+}
+
+// Future<String> signUp(String emailInput, String password) async {
+//   await Firebase.initializeApp();
+//   try {
+
+//   } on FirebaseAuthException catch (e) {
+//     if (e.code == 'user-not-found') {
+//       await FirebaseAuth.instance.createUserWithEmailAndPassword(
+//           email: emailInput, password: password);
+//       return await signUp(emailInput, password);
+//     } else if (e.code == 'wrong-password') {
+//       print('Wrong password provided for that user.');
+//       return null;
+//     }
+//   }
+//   return null;
+// }
