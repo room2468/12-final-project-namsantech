044f2ea4cb4f6d5b8238297b71bba10945598b6b
ITTECH-DEO <etechproffit@gmail.com>
2021-06-21 13:53:29 +0700

    Fix in Homepage

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,16 +1,19 @@
+import 'package:final_project/pages/LoginPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:final_project/animasi/ScaleRoute.dart';
-import 'package:final_project/pages/LogInPage.dart';
+import 'package:final_project/widgets/BestFoodWidget.dart';
+import 'package:final_project/widgets/BottomNavBarWidget.dart';
 import 'package:final_project/widgets/PopularFoodsWidget.dart';
 import 'package:final_project/widgets/SearchWidget.dart';
 import 'package:final_project/widgets/TopMenus.dart';
-import 'package:final_project/widgets/BottomNavBarWidget.dart';
-import 'package:final_project/widgets/BestFoodWidget.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage({user});
   @override
   _HomePageState createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
@@ -32,7 +35,9 @@ class _HomePageState extends State<HomePage> {
                 Icons.notifications_none,
                 color: Color(0xFF3a3737),
               ),
-              onPressed: () {Navigator.push(context, ScaleRoute(page: SignInPage()));})
+              onPressed: () {
+                Navigator.push(context, ScaleRoute(page: SignInPage()));
+              })
         ],
       ),
       body: SingleChildScrollView(
