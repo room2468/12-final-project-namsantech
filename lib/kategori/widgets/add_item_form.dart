33e8b74a052149ab2072279275f45aa0df79e2ed
ellafk <ellafitrikusumaningrum@gmail.com>
2021-06-22 18:55:59 +0700

    tambah item dan list firestore

---
+++ lib/kategori/widgets/add_item_form.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:final_project/kategori/utils/database.dart';
+
+class AddItemForm extends StatefulWidget {
+  final FocusNode kodeFocusNode;
+  final FocusNode kategoriFocusNode;
+
+  const AddItemForm({
+    this.kodeFocusNode,
+    this.kategoriFocusNode,
+  });
+
+  @override
+  _AddItemFormState createState() => _AddItemFormState();
+}
+
+class _AddItemFormState extends State<AddItemForm> {
+  final _addItemFormKey = GlobalKey<FormState>();
+
+  bool _isProcessing = false;
+
+  final TextEditingController _kodeController = TextEditingController();
+  final TextEditingController _kategoriController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _addItemFormKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 24.0),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: TextField(
+                    controller: _kodeController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      labelText: 'Kode Kategori',
+                      labelStyle:
+                          new TextStyle(color: Colors.black54, fontSize: 16.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      //
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: TextField(
+                    controller: _kategoriController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      labelText: 'Nama Kategori',
+                      labelStyle:
+                          new TextStyle(color: Colors.black54, fontSize: 16.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      //
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _isProcessing
+              ? Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: CircularProgressIndicator(
+                      // valueColor: AlwaysStoppedAnimation<Color>(
+                      //   //CustomColors.firebaseOrange,
+                      // ),
+                      ),
+                )
+              : Container(
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    color: Colors.teal[600],
+                    textColor: Theme.of(context).primaryColorLight,
+                    onPressed: () async {
+                      widget.kodeFocusNode.unfocus();
+                      widget.kategoriFocusNode.unfocus();
+
+                      if (_addItemFormKey.currentState.validate()) {
+                        setState(() {
+                          _isProcessing = true;
+                        });
+
+                        await DatabaseKategori.addItem(
+                          kode: _kodeController.text,
+                          kategori: _kategoriController.text,
+                        );
+
+                        setState(() {
+                          _isProcessing = false;
+                        });
+
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: 16.0, bottom: 16.0, left: 16, right: 16),
+                      child: Text(
+                        'ADD ITEM',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          // color: CustomColors.firebaseGrey,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
