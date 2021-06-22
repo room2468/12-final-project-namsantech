ed9cbb401c8fadf05313eb47f365b20f59821ec8
ITTECH-DEO <etechproffit@gmail.com>
2021-06-22 15:05:32 +0700

    Perubahan Pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:final_project/pages/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
-      home: SignInPage(),
-    ));
+      home: SignInPage(), 
+    ); 
+  }
+}
