eaadd75969df472b95e73015b0fbbe33e9638356
ITTECH-DEO <etechproffit@gmail.com>
2021-06-23 09:26:02 +0700

    View Gmaps

---
+++ lib/pages/google_map_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapScreen extends StatefulWidget {
+  @override
+  _GoogleMapScreenState createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  Set<Marker> _markers = {};
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _markers.add(
+        Marker(
+            markerId: MarkerId('id-1'),
+            position: LatLng(22.5448131, 88.3403691),
+            infoWindow: InfoWindow(
+              title: 'Offline Store',
+              snippet: 'Yuk Kesini',
+            )),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Map'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        markers: _markers,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(22.5448131, 88.3403691),
+          zoom: 15,
+        ),
+      ),
+    );
+  }
+}
