101ba9cea7c724210f787c0b91c4f80f3787afb7
ellafk <ellafitrikusumaningrum@gmail.com>
2021-06-25 14:35:02 +0700

    edit warna

--- lib/kategori/widgets/add_item_form.dart
+++ lib/kategori/widgets/add_item_form.dart
@@ -32,7 +32,7 @@ class _AddItemFormState extends State<AddItemForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _addItemFormKey,
-      child: Column(
+      child: ListView(
         children: [
           Padding(
             padding: const EdgeInsets.only(bottom: 24.0),
@@ -127,7 +127,7 @@ class _AddItemFormState extends State<AddItemForm> {
               : Container(
                   width: double.maxFinite,
                   child: RaisedButton(
-                    color: Colors.teal[600],
+                    color: Colors.yellow[600],
                     textColor: Theme.of(context).primaryColorLight,
                     onPressed: () async {
                       widget.nameFocusNode.unfocus();
@@ -160,7 +160,8 @@ class _AddItemFormState extends State<AddItemForm> {
                       child: Text(
                         'ADD ITEM',
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 18,
+                          color: Colors.black,
                           fontWeight: FontWeight.bold,
                           // color: CustomColors.firebaseGrey,
                           letterSpacing: 2,
